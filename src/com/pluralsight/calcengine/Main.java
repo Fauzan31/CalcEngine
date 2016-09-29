5dce74f72a3588cd5fc8c81a4acb6c499420c4b1
Fauzan31 <fauzanspeedly@gmail.com>
2016-09-30 00:21:08 +0700

    1.0

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,28 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        double val1 = 100.0d;
-        double val2 = 50.0d;
-        double result;
-        char opCode = 'd';
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+//        double val1 = 100.0d;
+//        double val2 = 50.0d;
+//        double result;
+//        char opCode = 'd';
 
-        if (opCode == 'a')
-            result = val1 + val2;
-        else if (opCode == 's')
-            result = val1 - val2;
-        else if (opCode == 'd')
-            result = val2 != 0.0d ? val1 / val2 : 0.0d;
+        for (int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = leftVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
 //            if (val2 != 0.0d)
 //                result = val1 / val2;
 //            else
 //                result = 0.0d;
-        else if (opCode == 'm')
-            result = val1 * val2;
-        else {
-            System.out.println("Error Invalid opCode");
-            result = 0.0d;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error Invalid opCode");
+                    results[i] = 0.0d;
+                    break;
+            }
         }
 
-        System.out.println(result);
+        for (double thResult : results) {
+            System.out.print("result = ");
+            System.out.println(thResult);
+        }
     }
 }
