6fe6253c4dc0052eabec67225d25468f77fdf76e
Fauzan31 <fauzanspeedly@gmail.com>
2016-10-03 12:07:45 +0700

    4.0

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -4,10 +4,21 @@ package com.pluralsight.calcengine;
  * Created by Fauzan on 10/2/2016.
  */
 public class MathEquation {
-    public double leftVal;
-    public double rightVal;
-    public char opCode;
-    public double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    public double getLeftVal() {return leftVal;}
+    public void setLeftVal(double leftVal){this.leftVal = leftVal;}
+
+    public double getRightVal() {return rightVal;}
+    public void setRightVal(double rightVal){this.rightVal = rightVal;}
+
+    public double getOpCode() {return opCode;}
+    public void setOpCode(char opCode){this.opCode = opCode;}
+
+    public double getResult(){return result;}
 
     public void execute(){
         switch (opCode) {
